0539912bc849e78bb34b8a4847397d14e505b242
Sadturtleman <rugsn12345@gmail.com>
2025-05-29 13:51:44 +0900

    movie info table create

---
+++ src/SQL/create table.sql
@@ -0,0 +1,15 @@
+create table movie_info(
+moviename varchar(255),
+movieengname varchar(255),
+createdate int,
+movietype varchar(255),
+genre varchar(255),
+moviestate varchar(255),
+director varchar(255),
+company varchar(255),
+movieid int auto_increment,
+primary key(movieid)
+);
+
+
+select * from movie_info;
